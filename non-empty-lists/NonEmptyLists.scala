c48a2d9469c05b9423b5f365bb94dca3d4b349e7
Todd O'Bryan <toddobryan@gmail.com>
2014-02-28 11:19:00 -0500

    almost finished with non-empty-lists

--- non-empty-lists/NonEmptyLists.scala
+++ non-empty-lists/NonEmptyLists.scala
@@ -15,7 +15,7 @@ object NonEmptyLists {
   /** given a ListOf[X], produces ...
   def listOfXFun(listOfX: ListOf[X]): ??? = {
     listOfX match {
-      case Empty() => ???
+      case Empty => ???
       case Cons(head, tail) => head  listOfXFun(tail)
     }
   } */
@@ -26,7 +26,7 @@ object NonEmptyLists {
   def neloxFun(nelox: NonEmptyListOf[X]): ??? = {
     nelox match {
       case Cons(head, Empty) => 
-      case Cons(head, tail) => 
+      case Cons(head, tail: NonEmptyListOf[X]) => 
     }
   } */
   
@@ -46,13 +46,114 @@ object NonEmptyLists {
       case _ => throw new Exception("must have at least two values in the list")
     }
   } */
+  
+  /** given a NonEmptyListOf[Double], produces the largest element in the list */
+  def max(nums: NonEmptyListOf[Double]): Double = {
+    nums match {
+      case Cons(head, Empty) => head
+      case Cons(head, tail: NonEmptyListOf[Double]) =>
+        val maxOfTail: Double = max(tail)
+        if (head > maxOfTail) head else maxOfTail
+    }
+  }
+  
+  /** given two Points, finds the distance between them */
+  def distance(p1: Point, p2: Point): Double = {
+    import math._
+    sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2))
+  }
+ 
+  /** given a list of at least 2 Points, produces the total distance you'd travel
+      to visit each in order */
+  def totalDistance(pts: ListOf[Point]): Double = {
+    pts match {
+      case Cons(one, Cons(two, Empty)) => distance(one, two)
+      case Cons(one, Cons(two, tail: NonEmptyListOf[Point])) => distance(one, two) + totalDistance(Cons(two, tail))
+      case _ => throw new Exception("must have at least two values in the list")
+    }
+  }
+
+  /** given a list of at least 2 Points, produces a picture connecting the points
+      on a white rectangle of size 100 by 100 */
+  def openPolygon(points: ListOf[Point]): Image = {
+    points match {
+      case Cons(one, Cons(two, Empty)) => 
+        RectangleFilled(Color.White, 100, 100).addLine(one.x, one.y, two.x, two.y)
+      case Cons(one, Cons(two, tail: NonEmptyListOf[Point])) => 
+        openPolygon(Cons(two, tail)).addLine(one.x, one.y, two.x, two.y)
+      case _ => throw new Exception("must have at least two values in the list")
+    }
+  }
+
+  /** given an item and a list, produces a new list with the item at the end */
+  def putAtEnd[X](x: X, listOfX: ListOf[X]): ListOf[X] = {
+    listOfX match {
+      case Empty => Cons(x, Empty)
+      case Cons(head, tail) => Cons(head, putAtEnd(x, tail))
+    }
+  }
+  
+  /** given a ListOf[X], produces a new list with the items reversed */
+  def reverse[X](listOfX: ListOf[X]): ListOf[X] = {
+    listOfX match {
+      case Empty => Empty
+      case Cons(head, tail) => putAtEnd(head, reverse(tail))
+    }
+  }
+  
+  
 }
 
 class TestNonEmptyLists extends FunSuite with Matchers {
   import NonEmptyLists._
 
   test("product") {
-    product(Cons(1, Empty)) shouldBe 1
-    product(Cons(3, Cons(4, Cons(2, Empty)))) shouldBe 24
+    product(Cons(1, Empty)) shouldEqual 1
+    product(Cons(3, Cons(4, Cons(2, Empty)))) shouldEqual 24
   }
+  
+  // uncomment each test as you write the functions
+  // make sure you write your own test cases!
+  /*
+  test("max") {
+    max(Cons(3.5, Empty)) shouldEqual 3.5
+    max(Cons(3.5, Cons(-2.7, Cons(5.1, Cons(3.4, Empty))))) shouldEqual 5.1
+  }
+  
+  test("distance") {
+    distance(Point(5, 5), Point(5, 5)) shouldEqual 0
+    distance(Point(5, 5), Point(8, 9)) shouldEqual 5.0
+    distance(Point(5, 5), Point(20, 30)) shouldEqual math.sqrt(850)
+  }
+  
+  test("totalDistance") {
+    the [Exception] thrownBy totalDistance(Empty) should have message "must have at least two values in the list"
+    the [Exception] thrownBy totalDistance(Cons(Point(0, 0), Empty)) should have message "must have at least two values in the list"
+    totalDistance(Cons(Point(5, 5), Cons(Point(8, 9), Empty))) shouldEqual 5.0
+    totalDistance(Cons(Point(5, 5), Cons(Point(8, 9), 
+        Cons(Point(3, -3), Cons(Point(0, 0), Empty))))) shouldEqual
+        18 + math.sqrt(18)
+  }
+  
+  test("openPolygon") {
+    the [Exception] thrownBy openPolygon(Empty) should have message "must have at least two values in the list"
+    the [Exception] thrownBy openPolygon(Cons(Point(0, 0), Empty)) should have message "must have at least two values in the list"
+    openPolygon(Cons(Point(0, 0), Cons(Point(50, 50), Empty))) shouldEqual Bitmap("poly1.png")
+    openPolygon(Cons(Point(10, 10), Cons(Point(20, 20), Cons(Point(10, 30), 
+        Cons(Point(20, 50), Cons(Point(5, 35), Empty)))))) shouldEqual Bitmap("poly2.png")
+  }
+  
+  test("putAtEnd") {
+    putAtEnd("a", Cons("z", Cons("y", Cons("x", Empty)))) shouldEqual 
+      Cons("z", Cons("y", Cons("x", Cons("a", Empty))))
+    putAtEnd(2, Empty) shouldEqual Cons(2, Empty)
+  }
+  
+  test("reverse") {
+    reverse(Empty) shouldEqual Empty
+    reverse(Cons("a", Cons("b", Cons("c", Empty)))) shouldEqual 
+        Cons("c", Cons("b", Cons("a", Empty)))
+  }
+  */
+
 }
